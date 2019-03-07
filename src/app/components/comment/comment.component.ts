e71b90e63f5dc265753d4eddd5bf31625050a10f
Salfay Anna Rita <salfay.anna@gmail.com>
2019-03-07 09:10:22 +0100

    feat(FEDEX): removed bug

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  GetPostandCommentService } from '../../services/getPostsandComments Service/get-postand-comment.service';
 
 @Component({
   selector: 'app-comment',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: GetPostandCommentService) { }
 
   ngOnInit() {
   }
 
+
+
+
+  vote(direction: string) {
+    const token = localStorage.getItem('token')
+    const id: string = direction['parentElement']['parentElement'].id;
+    if (direction['alt'] === 'upvote') {
+      this.service.votePost(id, 'true', `${token}`).subscribe( // authtokent átírni dinamikusra
+        data => {
+          if (data['status'] === 200) {
+            const currentNumber = parseInt(direction['nextElementSibling'].innerText, 0);
+            direction['nextElementSibling'].innerText = currentNumber + 1;
+          }
+        }
+      );
+    } else if (direction['alt'] === 'downvote') {
+      this.service.votePost(id, 'false', `${token}`).subscribe( // authtokent átírni dinamikusra
+        data => {
+          if (data['status'] === 200) {
+            const currentNumber = parseInt(direction['previousElementSibling'].innerText, 0);
+            direction['previousElementSibling'].innerText = currentNumber - 1;
+          }
+        }
+      );
+    }
+  }
 }
